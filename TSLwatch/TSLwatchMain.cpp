b4f433d146fae64a0c27c39ffe015845f9cb2abe
Pawel Krawczyk <pawel.krawczyk@hush.com>
2012-02-01 10:47:33 +0100

    * temporarily open Disallowed store (easy to remove, doesn't require admin)
    * added various checks and safe exits

--- TSLwatch/TSLwatchMain.cpp
+++ TSLwatch/TSLwatchMain.cpp
@@ -3,57 +3,52 @@
 #include "stdafx.h"
 
 // global structured defined here
-static struct options options;
-static struct CertList GlobalCertList;
-HANDLE hEventLog;
+static struct options options;			// from command line options
+static struct CertList GlobalCertList;	// this stores list of certificates to add to store
+HANDLE hEventLog;						// for logging
 
 // external functions
 extern BOOL insertIntoSystemStore(IN HCERTSTORE hSystemStore, IN LPTSTR certificate_base64);
 extern BOOL parseTSL(LPCWSTR url, TCertListPtr pCertList);
 
 int _tmain(int argc, _TCHAR* argv[]) {
-	//--------------------------------------------------------------------
-	// Declare and initialize variables.
-
 	HCERTSTORE  hSystemStore;              // system store handle
 	BOOL ret;
 	INT i;
 
+	// parse command line options
 	for(i=1; i<argc; i++) {
 		if(strncmp((const char*)argv[i], "-foreground", sizeof("foreground")) == 0)
 			options.foreground = true;
 		if(strncmp((const char*) argv[i], "-debug", sizeof("debug")) == 0)
 			options.debug = true;
 	}
-
+	
+	// initialize global certificate list structure
 	GlobalCertList.cert_list = NULL;
 	GlobalCertList.cert_list_index = 0L;
 
+	// register us at EventLog
 	hEventLog = RegisterEventSource(NULL, L"TSLwatch");
-
-	//hEventLog = OpenEventLog(NULL, L"Application");
 	if(hEventLog == NULL) {
-		GET_ERR(lpMsgBuf);
+		GET_ERR(lpMsgBuf);	// defined in tslwatch.h
 		wprintf(L"%s\n", lpMsgBuf);
 		exit(1);
 	}
 
 	// notify about start
-	ret = ReportEvent(hEventLog, EVENTLOG_INFORMATION_TYPE, CAT_GENERAL, TSLWATCH_START, 
-		NULL, 0, 0, NULL, NULL);
+	ret = ReportEvent(hEventLog, EVENTLOG_INFORMATION_TYPE, CAT_GENERAL, TSLWATCH_START, NULL, 0, 0, NULL, NULL);
 
 	// we are adding certs to Trusted Root store on Local Machine
 	// TSLwatch needs to run as a admin to access it
 	// otherwise each certificate will result in a confirmation prompt
-	hSystemStore = CertOpenStore(CERT_STORE_PROV_SYSTEM, 0, 0, 
-		CERT_SYSTEM_STORE_LOCAL_MACHINE, "Root");
+	//hSystemStore = CertOpenStore(CERT_STORE_PROV_SYSTEM, 0, 0, CERT_SYSTEM_STORE_LOCAL_MACHINE, "Root");
+	hSystemStore = CertOpenSystemStore(NULL, L"Disallowed");
 	if(hSystemStore == NULL) 	{
 		GET_ERR(lpMsgBuf);
 		
 		if(dw == ERROR_ACCESS_DENIED) { // most common cause
-			ret = ReportEvent(hEventLog, EVENTLOG_ERROR_TYPE, 
-				CAT_CERT_STORE, NEED_ADMIN, 
-			NULL, 0, 0, NULL, NULL);
+			ret = ReportEvent(hEventLog, EVENTLOG_ERROR_TYPE, CAT_CERT_STORE, NEED_ADMIN, NULL, 0, 0, NULL, NULL);
 			exit(1);
 		}
 
@@ -61,8 +56,7 @@ int _tmain(int argc, _TCHAR* argv[]) {
 		LPWSTR pInsertStrings[2] = {NULL, NULL};
 
 		pInsertStrings[0] = lpMsgBuf;
-		ReportEvent(hEventLog, EVENTLOG_ERROR_TYPE, CAT_CERT_STORE, 
-			GENERIC_ERROR, NULL, 1, 0, (LPCWSTR*)pInsertStrings, NULL);
+		ReportEvent(hEventLog, EVENTLOG_ERROR_TYPE, CAT_CERT_STORE, GENERIC_ERROR, NULL, 1, 0, (LPCWSTR*)pInsertStrings, NULL);
 
 		exit(1);
 	}
@@ -70,26 +64,34 @@ int _tmain(int argc, _TCHAR* argv[]) {
 #define EU_TSL  L"https://ec.europa.eu/information_society/policy/esignature/trusted-list/tl-mp.xml"
 	
 	ret = parseTSL(EU_TSL, &GlobalCertList);
+	if(ret == false) {
+		exit(1);
+	}
 
 	for(i=0;;) {
 		LPTSTR cert;
 		cert = GlobalCertList.cert_list[i];
 		if(cert == NULL)
 			break;
-		insertIntoSystemStore(hSystemStore, cert);
+		//insertIntoSystemStore(hSystemStore, cert);
+		wprintf(L"Add to store %s\n", cert); 
 		i += 1;
 	}
 
 	// When done using the store, close it.
 	ret = CertCloseStore(hSystemStore, 0);
-	if(FAILED(ret))
-	{
-		wprintf(L"Unable to close the CA system store.\n");
+	if(FAILED(ret)) 	{
+		GET_ERR(lpMsgBuf);
+		LPWSTR pInsertStrings[2] = {NULL, NULL};
+
+		pInsertStrings[0] = lpMsgBuf;
+		ReportEvent(hEventLog, EVENTLOG_ERROR_TYPE, CAT_CERT_STORE, GENERIC_ERROR, NULL, 1, 0, (LPCWSTR*)pInsertStrings, NULL);
+		
 		exit(1);
 	}
 
-	ret = ReportEvent(hEventLog, EVENTLOG_INFORMATION_TYPE, CAT_GENERAL, TSLWATCH_STOP, 
-		NULL, 0, 0, NULL, NULL);
+	// log termination
+	ret = ReportEvent(hEventLog, EVENTLOG_INFORMATION_TYPE, CAT_GENERAL, TSLWATCH_STOP, NULL, 0, 0, NULL, NULL);
 
 	CloseEventLog(hEventLog);
 
